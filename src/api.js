4dd5d050f8f267e02fceebd141bcf30c2e9c3738
xfstc <lfan@DESKTOP-M8NRRNC.localdomain>
2022-04-18 11:54:16 -0400

    Add edit, convert and delete fragment functions

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 
 // fragments microservice API
 const apiUrl = process.env.API_URL;
-
+export var data;
 /**
  * Given an authenticated user, request all fragments for this user from the
  * fragments microservice (currently only running locally). We expect a user
@@ -20,14 +20,14 @@ export async function getUserFragments(user) {
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
     }
-    const data = await res.json();
+    data = await res.json();
     console.log("Got user fragments data", { data });
   } catch (err) {
     console.error("Unable to call GET /v1/fragment", { err });
   }
 }
 
-export async function postUserFragments(user, fragment,type) {
+export async function postUserFragments(user, fragment, type) {
   try {
     const res = await fetch(`${apiUrl}/v1/fragments`, {
       method: "POST",
@@ -35,7 +35,7 @@ export async function postUserFragments(user, fragment,type) {
         "Content-Type": type,
         Authorization: `Bearer ${user.idToken}`,
       },
-      body: fragment,
+      body: type == "application/json" ? JSON.stringify(fragment) : fragment,
     });
     if (!res.ok) {
       throw new Error(`${res.status} ${res.statusText}`);
@@ -43,5 +43,60 @@ export async function postUserFragments(user, fragment,type) {
     await res.json();
   } catch (err) {
     console.error("Unable to call POST /v1/fragment", { err });
+  }
+}
+
+export async function deleteUserFragment(user, id) {
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    console.log(`Fragment(ID: ${id} has been deleted.)`);
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment", { err });
+  }
+}
+
+export async function getUserFragment(user, id) {
+  console.log("Requesting user fragment data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${user.idToken}`,
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return await res.blob();
+  } catch (err) {
+    console.error("Unable to call GET /v1/fragment", { err });
+  }
+}
+
+export async function editUserFragment(user, id,type, fragment) {
+  console.log("Editing user fragment data...");
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": type,
+        Authorization: `Bearer ${user.idToken}`,
+      },
+      body: type == "application/json" ? JSON.stringify(fragment) : fragment,
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    console.log(await res.text());
+  } catch (err) {
+    console.error("Unable to call PUT /v1/fragment", { err });
   }
 }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 // src/app.js
 
 import { Auth, getUser } from "./auth.js";
-import { getUserFragments, postUserFragments } from "./api.js";
+import {
+  getUserFragments,
+  postUserFragments,
+  data,
+  deleteUserFragment,
+  getUserFragment,
+  editUserFragment,
+} from "./api.js";
+import { off } from "process";
+import { stringify } from "querystring";
 
 async function init() {
   // Get our UI elements
@@ -30,20 +39,6 @@ async function init() {
     return;
   }
 
-  const form = document.getElementById("myForm");
-  form.onsubmit = submit;
-  function submit(event) {
-    var type = document.getElementById("type").value;
-    var fragment = document.getElementById("content").value;
-
-    postUserFragments(user, fragment, type);
-    event.preventDefault();
-  }
-
-  getFragments.onclick=()=>{
-    getUserFragments(user);
-  }
-
   // Log the user info for debugging purposes
   console.log({ user });
 
@@ -59,6 +54,176 @@ async function init() {
   // Do an authenticated request to the fragments API server and log the result
   getUserFragments(user);
 }
+
+// Post a new fragment
+const form = document.getElementById("myForm");
+form.onsubmit = submit;
+async function submit(event) {
+  const user = await getUser();
+  var type = document.getElementById("type").value;
+  var fragment;
+  var content = document.getElementById("content").value;
+  if (content.length != 0) {
+    fragment = content;
+  } else {
+    fragment = document.getElementById("image").files[0];
+  }
+
+  postUserFragments(user, fragment, type);
+  event.preventDefault();
+}
+
+// Edit fragment
+const editForm = document.getElementById("editForm");
+editForm.onsubmit = submitEdit;
+async function submitEdit(event) {
+  const user = await getUser();
+
+  var id = document.getElementById("fragmentID").value;
+  var type = document.getElementById("editType").value;
+  var fragment;
+  var content = document.getElementById("editContent").value;
+  if (content.length != 0) {
+    fragment = content;
+  } else {
+    fragment = document.getElementById("editFile").files[0];
+  }
+  event.preventDefault();
+
+  await editUserFragment(user, id, type, fragment);
+  event.preventDefault();
+}
+
+// Convert and render fragment  
+const convertForm = document.getElementById("convertForm");
+convertForm.onsubmit = submitConvert;
+async function submitConvert(event) {
+  const user = await getUser();
+  var type = document.getElementById("contentType").value;
+  var id =
+    document.getElementById("convertID").value +
+    document.getElementById("convertEXT").value;
+
+  event.preventDefault();
+  var converted = await getUserFragment(user, id);
+  var html = `<p>Converted fragment: </p>`;
+  if (
+    type == "text/markdown" ||
+    document.getElementById("convertEXT").value == ".txt"
+  ) {
+    var htmlSegment = `<div class="convertedFragment">
+    <p>${await converted.text()}</p></div>`;
+    console.log(`Converted fragment: ${await converted.text()}`);
+    html += htmlSegment;
+    var typeSegment = `<p>Content type after conversion: ${converted.type}</p>`;
+    html += typeSegment;
+  }
+  if (
+    type == "image/png" ||
+    type == "image/jpeg" ||
+    type == "image/webp" ||
+    type == "image/gif"
+  ) {
+    var htmlSegment = `<div class="convertedFragment">
+          <img src="${URL.createObjectURL(
+            converted
+          )}" alt="Converted Image Fragment">converted image fragment</img>
+        </div>`;
+    html += htmlSegment;
+    var typeSegment = `<p>Content type after conversion: ${converted.type}</p>`;
+    html += typeSegment;
+  }
+  var container = document.querySelector("#converted");
+  container.innerHTML = html;
+  event.preventDefault();
+}
+
+// List metadata for all fragments
+getFragments.onclick = async () => {
+  const user = await getUser();
+
+  await getUserFragments(user);
+
+  const body = document.body;
+  var tbl = document.createElement("table");
+  tbl.setAttribute("id", "myTable");
+  var table = document.getElementById("myTable");
+  if (table) {
+    table.parentNode.removeChild(table);
+  }
+  const th = tbl.insertRow();
+  const trID = th.insertCell();
+  trID.appendChild(document.createTextNode("Fragment ID"));
+  const trType = th.insertCell();
+  trType.appendChild(document.createTextNode("Type"));
+  const trSize = th.insertCell();
+  trSize.appendChild(document.createTextNode("Size"));
+
+  for (let i = 0; i < data.fragment.length; i++) {
+    const tr = tbl.insertRow();
+    const id = tr.insertCell();
+    id.appendChild(document.createTextNode(`${data.fragment[i].id}`));
+    const type = tr.insertCell();
+    type.appendChild(document.createTextNode(`${data.fragment[i].type}`));
+    const size = tr.insertCell();
+    size.appendChild(document.createTextNode(`${data.fragment[i].size}`));
+    var btnDelete = document.createElement("input");
+    btnDelete.type = "button";
+    btnDelete.value = "Delete";
+    // Delete fragment
+    btnDelete.onclick = async () => {
+      await deleteUserFragment(user, data.fragment[i].id);
+      await getUserFragments(user);
+      tbl.deleteRow(i + 1);
+    };
+    const btnDlt = tr.insertCell();
+    btnDlt.appendChild(btnDelete);
+    var btnView = document.createElement("input");
+    btnView.type = "button";
+    btnView.value = "View";
+    // Render fragment
+    btnView.onclick = async () => {
+      var viewData = await getUserFragment(user, data.fragment[i].id);
+      var html = `<p>The content of fragment: </p>`;
+      if (data.fragment[i].type == "text/plain") {
+        var htmlSegment = `<div class="fragment">
+        <p>${await viewData.text()}</p></div>`;
+        html += htmlSegment;
+      } else if (
+        data.fragment[i].type == "text/html" ||
+        data.fragment[i].type == "text/markdown"
+      ) {
+        var htmlSegment = `<div class="fragment">
+          <iframe src="${URL.createObjectURL(
+            viewData
+          )}" title="Html Fragment" height="400" width="600">${data.fragment[i].type} fragment</iframe>
+        </div>`;
+        html += htmlSegment;
+      } else if (data.fragment[i].type == "application/json") {
+        var htmlSegment = `<div class="fragment">
+          <p>${JSON.parse(await viewData.text())}</p></div>`;
+        html += htmlSegment;
+      } else if (
+        data.fragment[i].type == "image/png" ||
+        data.fragment[i].type == "image/jpeg" ||
+        data.fragment[i].type == "image/webp" ||
+        data.fragment[i].type == "image/gif"
+      ) {
+        var htmlSegment = `<div class="fragment">
+          <img src="${URL.createObjectURL(
+            viewData
+          )}" alt="Image Fragment">${data.fragment[i].type} fragment</img>
+        </div>`;
+        html += htmlSegment;
+      }
+      var container = document.querySelector("#container");
+      container.innerHTML = html;
+    };
+    const btnV = tr.insertCell();
+    btnV.appendChild(btnView);
+  }
+  body.appendChild(tbl);
+};
 
 // Wait for the DOM to be ready, then start the app
 addEventListener("DOMContentLoaded", init);
